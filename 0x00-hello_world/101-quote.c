49aec98b730b9a84b4dc69470e4e337139d80199
Nmmmmmnnnnmmmmmnnbbvcx <Kiptooemmanuel763@gmail.com>
2023-02-18 09:53:56 -0800

    tasks 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - entry point
- * Return: Always 1
+ *main - entry point
+ *Return: Always 1
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
-	return (1)
+	return (1);
 }
