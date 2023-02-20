3a00382a9b531abc54f57d33f3ba80483b52ef64
Nmmmmmnnnnmmmmmnnbbvcx <Kiptooemmanuel763@gmail.com>
2023-02-20 09:38:04 -0800

    tasks 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char c;
+    int i;
 
-    for (c = 'z'; c >= 'a'; c--)
-        putchar(c);
-
+    for (i = 122; i >= 97; i--)
+    {
+        putchar(i);
+    }
     putchar('\n');
-
     return (0);
 }
+
+
+
